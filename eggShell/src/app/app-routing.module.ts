69b2ed3d7c45891959d573e02f10a1d794cb058f
angyalz <angyalz79@gmail.com>
2021-11-18 19:02:37 +0100

    add components folder

--- eggShell/src/app/app-routing.module.ts
+++ eggShell/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEggComponent } from './components/add-egg/add-egg.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-egg', component: AddEggComponent },
+  { path: 'settings', component: SettingsComponent},
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- eggShell/src/app/app.module.ts
+++ eggShell/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './components/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { SettingsComponent } from './settings/settings.component';
-import { AddEggComponent } from './add-egg/add-egg.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AddEggComponent } from './components/add-egg/add-egg.component';
 
 @NgModule({
   declarations: [
