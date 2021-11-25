264750575bfdd5a2bb837731d7ac5ef1249f0bbc
angyalz <angyalz79@gmail.com>
2021-11-25 19:57:54 +0100

    fix match validator

--- eggShell/src/app/validators/match.validator.ts
+++ eggShell/src/app/validators/match.validator.ts
@@ -4,11 +4,22 @@ export function matchValidator(name1: string, name2: string): ValidatorFn {
 
     return (control: AbstractControl): ValidationErrors | null => {
 
+        console.log('MatchValidator control:', control, control.value)
+
         if (!control.value) {
             return null;
         }
 
-        return (<FormGroup>control.parent).get(name2)?.value !== (<FormGroup>control.parent).get(name1)?.value ? { match: true } : null;
+        if (
+            !(<FormGroup>control.parent).get(name1)?.value || 
+            !(<FormGroup>control.parent).get(name2)?.value ||
+            !(<FormGroup>control.parent).get(name1)?.dirty || 
+            !(<FormGroup>control.parent).get(name2)?.dirty 
+            ) {
+            return null
+        }
+
+        return (<FormGroup>control.parent).get(name1)?.value !== (<FormGroup>control.parent).get(name2)?.value ? { match: true } : null;
 
     }
 }
