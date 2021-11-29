723e95d46604fb1e86b49baefc2a8d1026280d77
angyalz <angyalz79@gmail.com>
2021-11-29 12:55:55 +0100

    add logs for barton debug

--- eggShell/src/app/components/barton/barton.component.ts
+++ eggShell/src/app/components/barton/barton.component.ts
@@ -10,7 +10,7 @@ import { MatMenuTrigger } from '@angular/material/menu';
 })
 export class BartonComponent implements OnInit {
 
-  isOpen: boolean = false;
+  // isOpen: boolean = false;
 
   URL = environment.apiUrl;
 
@@ -20,30 +20,36 @@ export class BartonComponent implements OnInit {
       species: 'tyúk',
       image: `${this.URL}images/poultries/hen.png`,
       qty: 6,
+      isOpen: false,
     },
     {
       species: 'kakas',
       image: `${this.URL}images/poultries/rooster2.png`,
       qty: 1,
+      isOpen: false,
     },
     {
       species: 'kacsa',
       image: `${this.URL}images/poultries/duck2.png`,
       qty: 2,
+      isOpen: false,
     },
     {
       species: 'gácsér',
       image: `${this.URL}images/poultries/duck1.png`,
       qty: 1,
+      isOpen: false,
     }
   ];
-  poultryToMove = [...this.poultry, this.isOpen];
-
+  poultryToMove = [...this.poultry];
+  // poultryToMove = [...this.poultry];
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  // this.barton = [...this.barton, this.isOpen];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -57,13 +63,20 @@ export class BartonComponent implements OnInit {
       );
     }
     this.poultryToMove = [...this.poultry];
-    console.log('event: ', event);
-    console.log('barton: ', this.barton);
-    console.log('poultry: ', this.poultry);
+    console.log('event at drop: ', event);
+    console.log('barton at drop: ', this.barton);
+    console.log('poultry at drop: ', this.poultry);
   }
 
-  openMenu() {
-
+  openMenu(i: number, item?: any) {   // debug item
+    // for (this.isOpen of this.barton) {
+    //   this.isOpen = false;
+    // }
+    console.log('barton: ', this.barton, i); // debug
+    console.log('item at openMenu: ', item); // debug
+    // console.log(cdkConnectedOverlayOpen); // debug
+    // this.barton[i].isOpen = !this.barton[i].isOpen;
+    // return this.barton[i].isOpen = !this.barton[i].isOpen;
   }
 
 }
