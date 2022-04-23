c708d05703be144ccbfcbbb8e55b2392fe9776f2
angyalz <angyalz79@gmail.com>
2022-04-23 19:55:10 +0200

    fe build v1

--- api/src/server.js
+++ api/src/server.js
@@ -13,7 +13,7 @@ const logger = require('./config/logger');
 // const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 const app = express();
-const staticUrl = '../public/eggShell';
+const staticUrl = '../public/angular';
 // const swaggerDocument = load('./docs/swagger.yaml');
 
 app.use(morgan('combined', { stream: logger.stream }));
@@ -30,29 +30,25 @@ app.use('/', (req, res, next) => {
 // app.post('/refresh', refresh);
 // app.post('/logout', logout);
 
-app.use('/', require('./controllers/auth/auth.routes'));
-app.use('/bartons', require('./controllers/bartons/bartons.routes'));
-// app.use('/eggs', require('./controllers/eggs/eggs.routes'));
-app.use('/poultry', require('./controllers/poultry/poultry.routes'));
-app.use('/share/invite', require('./controllers/share/invite/invite.routes'));
-app.use('/share/request', require('./controllers/share/request/request.routes'));
-app.use('/users', require('./controllers/users/users.routes'));
-app.get('/images/:file', (req, res, next) => {
+app.use('/api/', require('./controllers/auth/auth.routes'));
+app.use('/api/bartons', require('./controllers/bartons/bartons.routes'));
+// app.use('/api/eggs', require('./controllers/eggs/eggs.routes'));
+app.use('/api/poultry', require('./controllers/poultry/poultry.routes'));
+app.use('/api/share/invite', require('./controllers/share/invite/invite.routes'));
+app.use('/api/share/request', require('./controllers/share/request/request.routes'));
+app.use('/api/users', require('./controllers/users/users.routes'));
+app.get('/api/images/:file', (req, res, next) => {
     logger.info(`file request: ${req.params.file}`);
     res.download(`./public/images/${req.params.file}`);
 });
-app.get('/images/poultries/:file', (req, res, next) => {
+app.get('/api/images/poultries/:file', (req, res, next) => {
     logger.info(`file request: ${req.params.file}`);
     res.download(`./public/images/poultries/${req.params.file}`);
 });
 
 // app.use('/api-doc', serve, setup(swaggerDocument));
 
-app.use('/', express.static(staticUrl));
-
-app.all('*', (req, res) => {
-    res.redirect('');
-})
+app.use('*/*', express.static(staticUrl));
 
 app.use((err, req, res, next) => {
     logger.error(`ERROR ${err.statusCode}: ${err.message}`);
@@ -62,5 +58,9 @@ app.use((err, req, res, next) => {
         message: err.message
     })
 });
+
+app.all('*', (req, res) => {
+    res.redirect('');
+})
 
 module.exports = app;
